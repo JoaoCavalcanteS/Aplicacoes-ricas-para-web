08a9d97951b9b926f1266ec0e949e67d7197961d
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2023-03-01 22:29:58 -0300

    Form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var pesoTotal = 0;
+
 function allowDrop(ev){
     ev.preventDefault(); //não deixar o navegador arrastar os objetos
 
@@ -18,5 +20,30 @@ function drop(ev){
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    
+    if(ev.target.id ==='div2'){
+        // pesoTotal+=parseInt(ev.target.childNodes[0].getAttribute('data-peso'));
+        pesoTotal+=parseInt(document.getElementById(data).getAttribute('data-peso'));
+
+    }else if(ev.target.id === 'div1'){
+        // pesoTotal-=parseInt(ev.target.childNodes[0].getAttribute('data-peso'));
+        pesoTotal-=parseInt(document.getElementById(data).getAttribute('data-peso'));
+
+    };
+    
+    //exibindo o peso total na pagina
+    document.getElementById('pesoTotal').innerHTML = pesoTotal + "kg";
+}
+//adicionar ou subtrair o peso da imagem
+
+
+
+function dragEnd(){
+    // if(ev.target.parentNode.id != 'div2'){
+    //     pesoTotal -= parseInt(ev.target.getAttribute('data-peso'));
+    // }
+    // else{
+    //     document.getElementById('pesoTotal').innerHTML = pesoTotal + "kg";
+    // }
     
 }
